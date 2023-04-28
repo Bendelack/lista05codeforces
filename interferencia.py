f73c0b99f496efcddac2bc1d97df6c37100a438a
Rick Hill <rickk1010@gmail.com>
2023-04-28 15:34:12 -0300

    Questao de interferencia Finalizada

---
+++ interferencia.py
@@ -0,0 +1,6 @@
+a,b,c,d,e,f,g,h = map(int,input().split())
+
+if (a == 0 or a == 1) and (b == 0 or b == 1) and (c == 0 or c == 1) and (d == 0 or d == 1) and (e == 0 or e == 1) and (f == 0 or f == 1) and (g == 0 or g == 1) and (h == 0 or h == 1):
+    print('S')
+else:
+    print('F')
