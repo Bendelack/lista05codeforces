28bc59f205fec71f144553bcffbc66b8dc6908eb
Rick Hill <rickk1010@gmail.com>
2023-04-29 14:03:24 -0300

    Viagem no Tempo.

---
+++ tempo.py
@@ -0,0 +1,8 @@
+c1,c2,c3 = map(int,input().split())
+
+if c1 == c2 or c1 == c3 or c2 == c3:
+    print('S')
+elif c1 + c2 == c3 or c1 + c3 == c2 or  c2 + c3 == c1:
+    print('S')
+else:
+    print('N')
